eee494b7234c455d9e2592a80f9712e2014fb65e
Vilius Puškunalis <47086537+puskunalis@users.noreply.github.com>
2022-10-04 20:30:19 +0300

    Internet Technologies: Task 1-3: Use FluentWait for adding new entries to table

--- year-3/software-testing/task-1/task-3.py
+++ year-3/software-testing/task-1/task-3.py
@@ -2,7 +2,25 @@ import time
 import unittest
 
 from selenium import webdriver
+from selenium.common.exceptions import ElementNotVisibleException, ElementNotSelectableException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+def add_new_record(driver):
+    driver.find_element(By.ID, "addNewRecordButton").click()
+
+    driver.find_element(By.ID, "firstName").send_keys("FirstName")
+    driver.find_element(By.ID, "lastName").send_keys("LastName")
+    driver.find_element(By.ID, "userEmail").send_keys("email@example.com")
+    driver.find_element(By.ID, "age").send_keys("30")
+    driver.find_element(By.ID, "salary").send_keys("100000")
+    driver.find_element(By.ID, "department").send_keys("Test Department")
+
+    driver.find_element(By.ID, "submit").click()
+
+    next_button = driver.find_element(By.XPATH, "//button[text() = 'Next']")
+    return not next_button.get_property('disabled')
 
 class DemoQATest(unittest.TestCase):
 
@@ -25,6 +43,7 @@ class DemoQATest(unittest.TestCase):
         
         self.assertIsNotNone(driver.find_element(By.XPATH, "//div[@aria-valuenow = '100']"))
 
+        # Don't click the reset button too quickly as that bugs out the progress bar
         time.sleep(1)
 
         driver.find_element(By.XPATH, "//*[@id = 'resetButton']").click()
@@ -40,23 +59,14 @@ class DemoQATest(unittest.TestCase):
 
         driver.find_element(By.XPATH, "//span[text() = 'Web Tables']").click()
 
-        for _ in range(8):
-            driver.find_element(By.ID, "addNewRecordButton").click()
-
-            driver.find_element(By.ID, "firstName").send_keys("FirstName")
-            driver.find_element(By.ID, "lastName").send_keys("LastName")
-            driver.find_element(By.ID, "userEmail").send_keys("email@example.com")
-            driver.find_element(By.ID, "age").send_keys("30")
-            driver.find_element(By.ID, "salary").send_keys("100000")
-            driver.find_element(By.ID, "department").send_keys("Test Department")
-
-            driver.find_element(By.ID, "submit").click()
+        wait = WebDriverWait(driver, timeout=60, poll_frequency=0, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
+        wait.until(add_new_record)
 
         next_button = driver.find_element(By.XPATH, "//button[text() = 'Next']")
         driver.execute_script("arguments[0].scrollIntoView();", next_button)
         next_button.click()
 
-        driver.find_element(By.ID, "delete-record-11").click()
+        driver.find_element(By.XPATH, "//span[@Title = 'Delete']").click()
 
         total_pages = driver.find_element(By.XPATH, "//span[@class = '-totalPages']")
 
